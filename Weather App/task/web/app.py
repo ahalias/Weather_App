772c67c8284165a0acb5b5d05f9a5225697b8f4c
ahalias <haliasandrei@gmail.com>
2023-03-14 14:08:56 +0300

    fixed buggs

--- Weather App/task/web/app.py
+++ Weather App/task/web/app.py
@@ -15,7 +15,6 @@ Base = declarative_base()
 
 class City(Base):
     __tablename__ = "City"
-
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=False, nullable=False)
 
@@ -37,9 +36,8 @@ dict_with_weather_info = {}
 def get_id():
     return random.randrange(1000000)
 
-
 def api_fetch(city_query):
-    api_key = 'api'
+    api_key = '1e124edde75e0125e8273855822fb66c'
     req = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_query}&units=metric&appid={api_key}')
     return req
 
@@ -62,25 +60,20 @@ def render_cities():
             city_id = get_id()
             session.add(City(id=city_id, name=r["name"]))
             session.commit()
-            dict_with_weather_info[city_id] = {
-                "city": r["name"], "degrees": int(r["main"]["temp"]), "state": r["weather"][0]['main'],
-                "id": city_id
-                }
-
+            process_cities(r, city_id)
 
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         render_cities()
-        return render_template('index.html', cities=dict_with_weather_info)
     if request.method == "GET":
         if  len(dict_with_weather_info) == 0:
             for city, id in session.query(City.name, City.id).all():
                 req = api_fetch(city)
                 r = req.json()
                 process_cities(r, id)
-        return render_template('index.html', cities=dict_with_weather_info)
+    return render_template('index.html', cities=dict_with_weather_info)
 
 
 @app.route('/delete/<int:id>', methods=['POST'])
